504b38988d9ed774ff1be4156ea6afe6bfdca838
HuangYin <1071324110@qq.com>
2025-08-13 02:01:35 +0800

    init

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -7,4 +7,6 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 ############################################################
 # Runs
 ############################################################
-python pre_process_sysu.py --data_path /kaggle/.../SYSU_MM01_concise111
+# python pre_process_sysu.py --data_path /kaggle/.../SYSU_MM01_concise111
+
+python main.py --config_file "config/method.yml" TASK.NOTES=test TASK.NAMEtest MODEL.MODULE=Lucky
