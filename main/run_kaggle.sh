3ccb7364eb8319512c551c17f1b4f0eb83fed4e0
HuangYin <1071324110@qq.com>
2025-08-15 10:32:40 +0800

    V009-Baseline-resnet50_ibn_a

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -9,8 +9,8 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 ############################################################
 # python pre_process_sysu.py --data_path /kaggle/.../SYSU_MM01_concise111
 
-python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V007  MODEL.MODULE=Lucky
+# python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V007  MODEL.MODULE=Lucky
 
 
-python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V008  MODEL.MODULE=Lucky MODEL.BACKBONE_TYPE=resnet50_ibn_a
+python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V009  MODEL.MODULE=Lucky MODEL.BACKBONE_TYPE=resnet50_ibn_a
 
