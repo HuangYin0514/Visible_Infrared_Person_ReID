2bdbc201bd568d748228b8aae4345b128c34a921
HuangYin <1071324110@qq.com>
2025-08-18 16:48:30 +0800

    V019-Baseline

--- main/optimizer.py
+++ main/optimizer.py
@@ -8,44 +8,44 @@ class Optimizer:
 
     def load_optimizer(self, config, net):
         ################################################################################
-        # Ignored parameters
-        ignored_params = []
-        ignored_params += list(map(id, net.backbone_pooling.parameters()))
-        ignored_params += list(map(id, net.backbone_classifier.parameters()))
-
-        ################################################################################
-        # Base parameters
-        base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
-
-        ################################################################################
-        # Optimizer
-        optimizer = optim.SGD(
-            [
-                {"params": base_params, "lr": 0.1 * config.OPTIMIZER.LEARNING_RATE},
-                {"params": net.backbone_pooling.parameters(), "lr": config.OPTIMIZER.LEARNING_RATE},
-                {"params": net.backbone_classifier.parameters(), "lr": config.OPTIMIZER.LEARNING_RATE},
-            ],
-            weight_decay=5e-4,
-            momentum=0.9,
-            nesterov=True,
-        )
-        self.optimizer = optimizer
+        # # Ignored parameters
+        # ignored_params = []
+        # ignored_params += list(map(id, net.backbone_pooling.parameters()))
+        # ignored_params += list(map(id, net.backbone_classifier.parameters()))
 
         # ################################################################################
-        # # Ignored parameters
-        # base_params = []
-        # base_params += list(map(id, net.backbone.parameters()))
-        # ignored_params = filter(lambda p: id(p) not in base_params, net.parameters())
+        # # Base parameters
+        # base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
 
         # ################################################################################
         # # Optimizer
         # optimizer = optim.SGD(
         #     [
-        #         {"params": net.backbone.parameters(), "lr": 0.1 * config.OPTIMIZER.LEARNING_RATE},
-        #         {"params": ignored_params, "lr": config.OPTIMIZER.LEARNING_RATE},
+        #         {"params": base_params, "lr": 0.1 * config.OPTIMIZER.LEARNING_RATE},
+        #         {"params": net.backbone_pooling.parameters(), "lr": config.OPTIMIZER.LEARNING_RATE},
+        #         {"params": net.backbone_classifier.parameters(), "lr": config.OPTIMIZER.LEARNING_RATE},
         #     ],
         #     weight_decay=5e-4,
         #     momentum=0.9,
         #     nesterov=True,
         # )
         # self.optimizer = optimizer
+
+        ################################################################################
+        # Ignored parameters
+        base_params = []
+        base_params += list(map(id, net.backbone.parameters()))
+        ignored_params = filter(lambda p: id(p) not in base_params, net.parameters())
+
+        ################################################################################
+        # Optimizer
+        optimizer = optim.SGD(
+            [
+                {"params": net.backbone.parameters(), "lr": 0.1 * config.OPTIMIZER.LEARNING_RATE},
+                {"params": ignored_params, "lr": config.OPTIMIZER.LEARNING_RATE},
+            ],
+            weight_decay=5e-4,
+            momentum=0.9,
+            nesterov=True,
+        )
+        self.optimizer = optimizer
