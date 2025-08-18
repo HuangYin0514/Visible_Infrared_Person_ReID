3d773f8828e126936bcc7488a5a0d908c603868b
HuangYin <1071324110@qq.com>
2025-08-18 22:46:53 +0800

    V020-Baseline

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -10,4 +10,4 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # python pre_process_sysu.py --data_path /kaggle/.../SYSU_MM01_concise111
 
 # python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V017  MODEL.MODULE=Lucky MODEL.BACKBONE_TYPE=resnet50
-python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V019  MODEL.MODULE=Lucky MODEL.BACKBONE_TYPE=resnet50
+python main.py --config_file "config/method.yml" TASK.NAME=Baseline TASK.NOTES=V020  MODEL.MODULE=Lucky MODEL.BACKBONE_TYPE=resnet50
