e6144ae6014ced85aca7fb2394a3e2951fe79660
HuangYin <1071324110@qq.com>
2025-08-19 01:04:58 +0800

    V024-Baseline

--- main/optimizer.py
+++ main/optimizer.py
@@ -17,10 +17,13 @@ class Optimizer:
         for module in special_modules:
             ignored_params += list(map(id, module.parameters()))
 
+        # Base parameters
         base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
 
+        # Param groups
         param_groups = [{"params": base_params, "lr": 0.1 * config.OPTIMIZER.LEARNING_RATE}]
         for module in special_modules:
             param_groups.append({"params": module.parameters(), "lr": config.OPTIMIZER.LEARNING_RATE})
 
+        # Optimizer
         self.optimizer = optim.SGD(param_groups, weight_decay=5e-4, momentum=0.9, nesterov=True)
