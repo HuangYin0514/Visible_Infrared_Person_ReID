d1f4b863a041b1efe4141e13f340f9c9c768282c
HuangYin <1071324110@qq.com>
2025-09-01 10:43:47 +0800

    V098

--- Ablation/B_I_C_IP/util/os_walk.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-
-def os_walk(folder_dir):
-    for root, dirs, files in os.walk(folder_dir):
-        files = sorted(files, reverse=True)
-        dirs = sorted(dirs, reverse=True)
-        return root, dirs, files
--- Ablation/baseline/util/make_dirs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def make_dirs(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-        print("Successfully make dirs: {}".format(dir))
-    else:
-        print("Existed dirs: {}".format(dir))
