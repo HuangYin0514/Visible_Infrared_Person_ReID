d988df2fa21eaf05fb5ba7e0b2f793330f29a798
HuangYin <1071324110@qq.com>
2025-09-05 18:05:56 +0800

    V115

--- main/model.py
+++ main/model.py
@@ -148,20 +148,20 @@ class Modal_Interaction(nn.Module):
         inter_c_dim = int(c_dim // r)
 
         self.MAMBA = CrossModalMamba(in_cdim=c_dim, hidden_cdim=256)
-        self.att = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(c_dim, inter_c_dim, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(inter_c_dim),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(inter_c_dim, c_dim, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(c_dim),
-        )
-        self.sigmoid = nn.Sigmoid()
+        # self.att = nn.Sequential(
+        #     nn.AdaptiveAvgPool2d(1),
+        #     nn.Conv2d(c_dim, inter_c_dim, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(inter_c_dim),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(inter_c_dim, c_dim, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(c_dim),
+        # )
+        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, vis_feat, inf_feat):
         vis_mamba_feat, inf_mamba_feat = self.MAMBA(vis_feat, inf_feat)
-        vis_feat = vis_feat + inf_feat * self.sigmoid(self.att(inf_mamba_feat))
-        inf_feat = inf_feat + vis_feat * self.sigmoid(self.att(vis_mamba_feat))
+        vis_feat = vis_feat + inf_feat * inf_mamba_feat
+        inf_feat = inf_feat + vis_feat * vis_mamba_feat
         return vis_feat, inf_feat
 
 
