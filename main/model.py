a1f5565bee6ab1c9bc37cf7ebecaa021b7fd6b91
HuangYin <1071324110@qq.com>
2025-09-18 18:06:29 +0800

    V162

--- main/model.py
+++ main/model.py
@@ -24,11 +24,6 @@ class ReIDNet(nn.Module):
         self.backbone_pooling = GeneralizedMeanPoolingP()
         self.backbone_classifier = Classifier(BACKBONE_FEATURES_DIM, n_class)
 
-        # ------------- modal propagation -----------------------
-        self.modal_propagation_pooling = GeneralizedMeanPoolingP()
-        self.modal_propagation_classifier = Classifier(BACKBONE_FEATURES_DIM, n_class)
-        self.modal_propagation = DistillKL(T=4)
-
     def forward(self, x_vis, x_inf, modal):
         backbone_feature_map = self.backbone(x_vis, x_inf, modal)
 
