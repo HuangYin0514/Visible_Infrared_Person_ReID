3960d02835b8cec7b428e815d7f2f669f2f7a903
HuangYin <1071324110@qq.com>
2025-09-23 00:17:05 +0800

    V183

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -12,5 +12,5 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 
 # python main.py --config_file "config/method.yml" TASK.NOTES=V106 TASK.NAME=B_IP OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_INTERACTION_FLAG=False MODEL.MODAL_CALIBRATION_FLAG=False MODEL.MODAL_PROPAGATION_FALG=True
 
-python main.py --config_file "config/method.yml" TASK.NOTES=V182 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 
+python main.py --config_file "config/method.yml" TASK.NOTES=V183 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 
 
