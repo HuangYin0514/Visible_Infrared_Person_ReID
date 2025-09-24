bcc503e2731edd4713688602c9bfee29f4332986
HuangYin <1071324110@qq.com>
2025-09-24 14:14:33 +0800

    V187

--- main/model/model_main_tool.py
+++ main/model/model_main_tool.py
@@ -2,6 +2,7 @@ import copy
 
 import torch
 import torch.nn as nn
+from einops import rearrange
 from torch.nn import functional as F
 
 from .mamba import MAMBA
@@ -13,32 +14,11 @@ class Interaction(nn.Module):
     def __init__(self):
         super(Interaction, self).__init__()
 
-        self.part_num = 6
-
-        self.vis_part_pool = nn.ModuleList()
-        self.vis_part_att = nn.ModuleList()
-        for i in range(self.part_num):
-            # self.vis_part_pool.append(nn.AdaptiveMaxPool2d((1, 1)))
-            self.vis_part_pool.append(nn.AdaptiveAvgPool2d((1, 1)))
-            self.vis_part_att.append(
-                nn.Sequential(
-                    nn.Conv2d(2048, 2048, kernel_size=1, stride=1, padding=0),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(2048, 2048, kernel_size=1, stride=1, padding=0),
-                    nn.Sigmoid(),
-                )
-            )
-
-        self.inf_part_pool = copy.deepcopy(self.vis_part_pool)
-        self.inf_part_att = copy.deepcopy(self.vis_part_att)
-
-        # self.mamba = nn.Sequential(
-        #     nn.Conv2d(2048, 2048, kernel_size=1, stride=1, padding=0),
-        #     nn.BatchNorm2d(2048),
-        #     nn.ReLU(inplace=True),
-        # )
-
         self.mamba = MAMBA(in_cdim=2048, hidden_cdim=256)
+
+        self.pooling = nn.AdaptiveAvgPool2d((6, 3))
+        # self.un_pooling = nn.Upsample(scale_factor=3, mode="bilinear", align_corners=True)
+        # F.interpolate(f_i, size=size_in, mode="nearest")
 
         self.vis_add_inf = nn.Sequential(
             nn.Conv2d(2048, 2048, kernel_size=1, stride=1, padding=0),
@@ -57,34 +37,29 @@ class Interaction(nn.Module):
         vis_feat_map, inf_feat_map = torch.chunk(feat_map, 2, dim=0)
 
         # Split Horizon Strategy & Patch mixed reordering
-        vis_part_feat_map = torch.chunk(vis_feat_map, self.part_num, dim=2)  # list(self.part_num)
-        inf_part_feat_map = torch.chunk(inf_feat_map, self.part_num, dim=2)
+        vis_part_feat_map = self.pooling(vis_feat_map)  # [B, C, 6, 3]
+        vis_part_rearrange_feat_map = rearrange(vis_part_feat_map, "b c h w -> b c (h w)")
+        inf_part_feat_map = self.pooling(inf_feat_map)
+        inf_part_rearrange_feat_map = rearrange(inf_part_feat_map, "b c h w -> b c (h w)")
 
         mixed_feat = []
-        for i in range(self.part_num):
-            mixed_feat.append(self.vis_part_pool[i](vis_part_feat_map[i]).squeeze(-1))
-            mixed_feat.append(self.inf_part_pool[i](inf_part_feat_map[i]).squeeze(-1))
-        mixed_feat = torch.stack(mixed_feat, dim=2)  # [B//2, C, self.part_num * 2, 1]
+        for i in range(6 * 3):
+            mixed_feat.append(vis_part_rearrange_feat_map[:, :, i])
+            mixed_feat.append(inf_part_rearrange_feat_map[:, :, i])
+        mixed_feat = torch.stack(mixed_feat, dim=2).unsqueeze(3)  # [B//2, C, self.part_num * 2, 1]
 
         # Mamba
-        mamba_feat = self.mamba(mixed_feat)  # [B//2, C, self.part_num * 2, 1]
-        vis_mamba_feat = mamba_feat[:, :, 0::2]  # [B//2, C, self.part_num, 1]
+        mamba_feat = self.mamba(mixed_feat)  # [B//2, C, num_patch, 1]
+        vis_mamba_feat = mamba_feat[:, :, 0::2]  # [B//2, C, num_patch, 1]
+        vis_mamba_feat = rearrange(vis_mamba_feat.squeeze(), "b c (h w)-> b c h w", h=6, w=3)  # [B//2, C, 6, 3]
+        vis_mamba_feat = F.interpolate(vis_mamba_feat, size=(H, W), mode="nearest")  # [B//2, C, H, W]
         inf_mamba_feat = mamba_feat[:, :, 1::2]
-
-        # Weighted Fusion
-        vis_weighted_feat_map = []  # [B//2, C, H, W]
-        inf_weighted_feat_map = []
-        for i in range(self.part_num):
-            vis_weight_i = self.vis_part_att[i](vis_mamba_feat[:, :, i].unsqueeze(-1))
-            inf_weight_i = self.inf_part_att[i](inf_mamba_feat[:, :, i].unsqueeze(-1))
-            vis_weighted_feat_map.append(vis_weight_i * vis_part_feat_map[i])
-            inf_weighted_feat_map.append(inf_weight_i * inf_part_feat_map[i])
-        vis_weighted_feat_map = torch.cat(vis_weighted_feat_map, dim=2)
-        inf_weighted_feat_map = torch.cat(inf_weighted_feat_map, dim=2)
+        inf_mamba_feat = rearrange(inf_mamba_feat.squeeze(), "b c (h w)-> b c h w", h=6, w=3)
+        inf_mamba_feat = F.interpolate(inf_mamba_feat, size=(H, W), mode="nearest")
 
         # Fusion
-        vis_feat_map = self.vis_add_inf(inf_weighted_feat_map) + vis_feat_map
-        inf_feat_map = self.inf_add_vis(vis_weighted_feat_map) + inf_feat_map
+        vis_feat_map = self.vis_add_inf(inf_mamba_feat) + vis_feat_map
+        inf_feat_map = self.inf_add_vis(vis_mamba_feat) + inf_feat_map
         feat_map = torch.cat([vis_feat_map, inf_feat_map], dim=0)
         return feat_map
 
