1611dec57aba28b88d82092b98eb04869c4a311c
HuangYin <1071324110@qq.com>
2025-09-30 10:33:38 +0800

    V208

--- main/model/mamba.py
+++ main/model/mamba.py
@@ -42,10 +42,10 @@ class CS_MAMBA(nn.Module):
         self.vi_featmap_2_patch = Featmap_2_Patch()
         self.mamba = Mamba(in_cdim=in_cdim * 2, d_model=d_model)
         self.vi_patch_2_featmap = Patch_2_Featmap()
-        self.norm_2 = nn.LayerNorm(in_cdim * 2)
 
+        self.norm_2 = nn.LayerNorm(POOL_HEGHT * POOL_WIDTH)
         self.attention = nn.Sequential(
-            nn.Linear(2048 * 2, 2048 * 2, bias=False),
+            nn.Linear(POOL_HEGHT * POOL_WIDTH, 1, bias=False),
             nn.Sigmoid(),
         )
 
@@ -67,18 +67,17 @@ class CS_MAMBA(nn.Module):
         vi_feat_patch = self.vi_featmap_2_patch(vi_feat_map)  # [B, 2C, n_patch]
         vi_feat_patch = rearrange(vi_feat_patch, "B D L -> B L D")  # [B, n_patch, 2C]
         vi_feat_patch = self.mamba(self.norm_1(vi_feat_patch)) + vi_feat_patch  # [B, n_patch, 2C]
-        vi_feat_patch = self.norm_2(vi_feat_patch)
         vi_feat_patch = rearrange(vi_feat_patch, "B L D -> B D L")  # [B, 2C, n_patch]
 
         # --- Attention ---
-        vi_feat_patch = vi_feat_patch.mean(2)  # [B, 2C]
-        vi_attention = self.attention(vi_feat_patch)  # [B, 2C]
-        vis_attention = vi_attention[:, 0::2].unsqueeze(2).unsqueeze(3)  # [B, 2C, 1, 1]
-        inf_attention = vi_attention[:, 1::2].unsqueeze(2).unsqueeze(3)
+        vi_feat_patch = self.norm_2(vi_feat_patch)
+        vi_attention = self.attention(vi_feat_patch)  # [B, 2C, 1]
+        # vis_attention, inf_attention = vi_attention.split(split_size=[C, C], dim=1)
+        vis_attention, inf_attention = vi_attention[:, 0::2, :], vi_attention[:, 1::2, :]
 
         # ---- FFN ----
-        out_vis = self.ffn_vis(vis_attention * vis_feat_map)
-        out_inf = self.ffn_inf(inf_attention * inf_feat_map)
+        out_vis = self.ffn_vis(vis_attention.unsqueeze(3) * vis_feat_map)
+        out_inf = self.ffn_inf(inf_attention.unsqueeze(3) * inf_feat_map)
         return out_vis, out_inf
 
 
