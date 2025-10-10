ec9d1a5f99be54522a960ba5ef2b5358bd24aaa8
HuangYin <1071324110@qq.com>
2025-10-10 17:53:10 +0800

    V212

--- main/data/data_loader.py
+++ main/data/data_loader.py
@@ -55,6 +55,9 @@ class Data_Loder:
             query_img, query_label, query_cam = process_query_sysu(config.DATASET.TRAIN_DATASET_PATH, mode=config.DATASET.MODE)
             gallery_img, gallery_label, gallery_cam = process_gallery_sysu(config.DATASET.TRAIN_DATASET_PATH, mode=config.DATASET.MODE, trial=0)
 
+        elif config.DATASET.TRAIN_DATASET == "reg_db":
+            pass
+
         queryset = TestDataset(query_img, query_label, transform=transform_test, img_size=config.DATALOADER.IMAGE_SIZE)
         gallset = TestDataset(gallery_img, gallery_label, transform=transform_test, img_size=config.DATALOADER.IMAGE_SIZE)
 
@@ -118,6 +121,68 @@ class Dataset4Sysu_mm01(data.Dataset):
 
         img1, target1 = self.color_image[self.cIndex[index]], self.color_label[self.cIndex[index]]
         img2, target2 = self.thermal_image[self.tIndex[index]], self.thermal_label[self.tIndex[index]]
+
+        img1 = self.transform(img1)
+        img2 = self.transform(img2)
+
+        return img1, img2, target1, target2
+
+    def __len__(self):
+        return len(self.train_color_label)
+
+
+def load_data(input_data_path):
+    with open(input_data_path) as f:
+        data_file_list = open(input_data_path, "rt").read().splitlines()
+        # Get full list of image and labels
+        file_image = [s.split(" ")[0] for s in data_file_list]
+        file_label = [int(s.split(" ")[1]) for s in data_file_list]
+
+    return file_image, file_label
+
+
+class RegDBData(data.Dataset):
+    def __init__(self, data_dir, trial, transform=None, colorIndex=None, thermalIndex=None):
+        # Load training images (path) and labels
+        data_dir = "../Datasets/RegDB/"
+        train_color_list = data_dir + "idx/train_visible_{}".format(trial) + ".txt"
+        train_thermal_list = data_dir + "idx/train_thermal_{}".format(trial) + ".txt"
+
+        color_img_file, train_color_label = load_data(train_color_list)
+        thermal_img_file, train_thermal_label = load_data(train_thermal_list)
+
+        train_color_image = []
+        for i in range(len(color_img_file)):
+            img = Image.open(data_dir + color_img_file[i])
+            img = img.resize((144, 288), Image.ANTIALIAS)
+            pix_array = np.array(img)
+            train_color_image.append(pix_array)
+        train_color_image = np.array(train_color_image)
+
+        train_thermal_image = []
+        for i in range(len(thermal_img_file)):
+            img = Image.open(data_dir + thermal_img_file[i])
+            img = img.resize((144, 288), Image.ANTIALIAS)
+            pix_array = np.array(img)
+            train_thermal_image.append(pix_array)
+        train_thermal_image = np.array(train_thermal_image)
+
+        # BGR to RGB
+        self.train_color_image = train_color_image
+        self.train_color_label = train_color_label
+
+        # BGR to RGB
+        self.train_thermal_image = train_thermal_image
+        self.train_thermal_label = train_thermal_label
+
+        self.transform = transform
+        self.cIndex = colorIndex
+        self.tIndex = thermalIndex
+
+    def __getitem__(self, index):
+
+        img1, target1 = self.train_color_image[self.cIndex[index]], self.train_color_label[self.cIndex[index]]
+        img2, target2 = self.train_thermal_image[self.tIndex[index]], self.train_thermal_label[self.tIndex[index]]
 
         img1 = self.transform(img1)
         img2 = self.transform(img2)
