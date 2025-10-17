5cc46641e08ca71060a4ade13d0901c12c5adb0e
HuangYin <1071324110@qq.com>
2025-10-17 10:47:54 +0800

    V244

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -8,9 +8,7 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # Runs
 ############################################################
 # python pre_process_sysu.py --data_path /kaggle/.../SYSU_MM01_concise111
-#  OPTIMIZER.LEARNING_RATE=0.0003
 
-# python main.py --config_file "config/method.yml" TASK.NOTES=V106 TASK.NAME=B_IP OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_INTERACTION_FLAG=False MODEL.MODAL_CALIBRATION_FLAG=False MODEL.MODAL_PROPAGATION_FALG=True
+# python main.py --config_file "config/method.yml" TASK.NOTES=V243 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 
 
-python main.py --config_file "config/method.yml" TASK.NOTES=V243 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 
-
+python main.py --config_file "config/method.yml" TASK.NOTES=V244 TASK.NAME=B_I_C_P_regdb_8 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 DATASET.TRAIN_DATASET=reg_db DATASET.TRAIN_DATASET_PATH=/kaggle/input/reg-db/RegDB/ DATASET.TRIAL=8 MODEL.NON_LOCAL_FLAG=False
