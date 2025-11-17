40f15297e0edc4342b4eed76331a0d88e6d07d1d
HuangYin <1071324110@qq.com>
2025-11-18 01:42:34 +0800

    V313

--- main/main.py
+++ main/main.py
@@ -168,7 +168,7 @@ def run(config):
 
                 # ---- Propagation  ----
                 modal_propagation_loss = net.propagation(student_logits=global_cls_score, teacher_logits=calibration_cls_score)
-                total_loss += config.MODEL.MODAL_PROPAGATION_WEIGHT * modal_propagation_loss
+                total_loss += config.MODEL.MODAL_PROPAGATION_WEIGHT * modal_propagation_loss / B
                 meter.update({"modal_propagation_loss": modal_propagation_loss.item()})
 
                 optimizer.zero_grad()
