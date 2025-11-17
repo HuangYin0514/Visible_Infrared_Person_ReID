06b208ccd924c50d44749798178ca4ef1498529c
HuangYin <1071324110@qq.com>
2025-11-17 11:07:38 +0800

    V312

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -11,8 +11,9 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 ############################################################
 # python main.py --config_file "config/method.yml" TASK.NOTES=V310 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.01
 
-python main.py --config_file "config/method.yml" TASK.NOTES=V311 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.02
+# python main.py --config_file "config/method.yml" TASK.NOTES=V311 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.02
 
+python main.py --config_file "config/method.yml" TASK.NOTES=V312 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.03
 
 ############################################################
 # Reg_db
