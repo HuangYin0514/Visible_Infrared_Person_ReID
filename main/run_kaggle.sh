6594e658436dabf96381f0edfd7d88a6fc9b17e5
HuangYin <1071324110@qq.com>
2025-12-03 20:21:23 +0800

    V363

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -25,7 +25,7 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 mkdir -p results/outputs/models
 gdown -O results/outputs/models/model_48.pth 1fbiBLdseQU9uJSGnEzu_JZNIdrZbVYhK 
 python vis_main.py --config_file "config/method.yml" TASK.NOTES=V363 TASK.NAME=visualization TASK.MODE=visualization MODEL.RESUME_EPOCH=48
-# tar -czf ../result_method.tar.gz results
+tar -czf ../result_method.tar.gz results
 # rm -rf results/outputs/*
 # ***********************************************************************************************************************************************
 
