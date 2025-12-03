497dc791b4a7824794c8284c7368b3e9686ae0ba
HuangYin <1071324110@qq.com>
2025-12-03 20:21:01 +0800

    V363

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -25,8 +25,8 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 mkdir -p results/outputs/models
 gdown -O results/outputs/models/model_48.pth 1fbiBLdseQU9uJSGnEzu_JZNIdrZbVYhK 
 python vis_main.py --config_file "config/method.yml" TASK.NOTES=V363 TASK.NAME=visualization TASK.MODE=visualization MODEL.RESUME_EPOCH=48
-tar -czf ../result_method.tar.gz results
-rm -rf results/outputs/*
+# tar -czf ../result_method.tar.gz results
+# rm -rf results/outputs/*
 # ***********************************************************************************************************************************************
 
 # python main.py --config_file "config/method.yml" TASK.NOTES=V363 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODULE=B
