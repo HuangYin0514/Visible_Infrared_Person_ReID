d2875a9ea2b9137698ad1e3282182d208340b53a
HuangYin <1071324110@qq.com>
2025-12-04 10:41:42 +0800

    v367

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -18,10 +18,10 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 
 # python main.py --config_file "config/method.yml" TASK.NOTES=V362 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.4 
 
+# B_I_C_P / sysu-mm01 / all-search 
 # python main.py --config_file "config/method.yml" TASK.NOTES=v364 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.4
-## Visualization "Lucky" 
 # # 无法下载Google大内容： wget -O results/outputs/models/model_48.pth "https://drive.google.com/file/d/1fbiBLdseQU9uJSGnEzu_JZNIdrZbVYhK/view?usp=sharing"
-# ***********************************************************************************************************************************************
+# Visualization *********************************************************************************************************************************
 # mkdir -p results/outputs/models
 # gdown -O results/outputs/models/model_48.pth 1fbiBLdseQU9uJSGnEzu_JZNIdrZbVYhK 
 # python vis_main.py --config_file "config/method.yml" TASK.NOTES=V363 TASK.NAME=visualization TASK.MODE=visualization MODEL.RESUME_EPOCH=48
@@ -29,9 +29,12 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # rm -rf results/outputs/*
 # ***********************************************************************************************************************************************
 
-python main.py --config_file "config/method.yml" TASK.NOTES=v366 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODULE=B
-## Visualization "Lucky" 
-# ***********************************************************************************************************************************************
+# B_I_C_P / sysu-mm01 / indoor
+python main.py --config_file "config/method.yml" TASK.NOTES=v367 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.4 DATASET.MODE=indoor
+
+# Baseline
+# python main.py --config_file "config/method.yml" TASK.NOTES=v366 TASK.NAME=B_I_C_P OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODULE=B
+# Visualization *********************************************************************************************************************************
 # mkdir -p results/outputs/models
 # gdown -O results/outputs/models/model_48.pth 1iRFI_pq9UyXz7o3wqxJ7r2eBVLrI0_st 
 # python vis_main.py --config_file "config/method.yml" TASK.MODE=visualization MODEL.RESUME_EPOCH=48 MODEL.MODULE=B
