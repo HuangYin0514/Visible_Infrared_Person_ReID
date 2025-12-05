c99a2ce6e20467a52a6afb67e7372624f05b9d81
HuangYin <1071324110@qq.com>
2025-12-05 21:33:37 +0800

    v375

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -37,8 +37,9 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # B_I_IP / sysu-mm01 / all-search  ===================================================================================================================
 # python main.py --config_file "config/method.yml" TASK.NOTES=V372 TASK.NAME=B_IC_IP OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.4 MODEL.MODULE=B_IC_IP
 
-# B_I_IP / sysu-mm01 / all-search / Parameter Analysis ===================================================================================================================
-python main.py --config_file "config/method.yml" TASK.NOTES=v374 TASK.NAME=B_I_C_P_mpw01 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.1
+# B_I_C_P / sysu-mm01 / all-search / Parameter Analysis ===================================================================================================================
+# python main.py --config_file "config/method.yml" TASK.NOTES=v374 TASK.NAME=B_I_C_P_mpw01 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.1
+python main.py --config_file "config/method.yml" TASK.NOTES=v375 TASK.NAME=B_I_C_P_mpw01 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.2
 
 ############################################################
 # Reg_db
