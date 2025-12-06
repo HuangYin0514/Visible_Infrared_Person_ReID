76971b4f647aff044af924a6c0610591a678de1c
HuangYin <1071324110@qq.com>
2025-12-06 22:39:19 +0800

    v378

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -41,7 +41,8 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # python main.py --config_file "config/method.yml" TASK.NOTES=v374 TASK.NAME=B_I_C_P_mpw01 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.1
 # python main.py --config_file "config/method.yml" TASK.NOTES=v375 TASK.NAME=B_I_C_P_mpw02 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.2
 # python main.py --config_file "config/method.yml" TASK.NOTES=v376 TASK.NAME=B_I_C_P_mpw03 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.3
-python main.py --config_file "config/method.yml" TASK.NOTES=v377 TASK.NAME=B_I_C_P_mpw05 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.5
+# python main.py --config_file "config/method.yml" TASK.NOTES=v377 TASK.NAME=B_I_C_P_mpw05 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.5
+python main.py --config_file "config/method.yml" TASK.NOTES=v378 TASK.NAME=B_I_C_P_mpw06 OPTIMIZER.TOTAL_TRAIN_EPOCH=61 MODEL.MODAL_PROPAGATION_WEIGHT=0.6
 
 ############################################################
 # Reg_db
